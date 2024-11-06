8baa26ca50bf85dcb3590d7f049b203642f953ff
kwelzenbach <kaggx6@gmail.com>
2024-11-05 18:10:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -80,9 +80,9 @@ function showSubmissionForm() {
             <label for="classHour">Hour:</label>
             <select id="classHour" required>
                 <option value="">Select Hour</option>
-                <option value="4th">1</option>
-                <option value="5th">2</option>
-                <option value="7th">3</option>
+                <option value="1">4th</option>
+                <option value="2">5th</option>
+                <option value="3">7th</option>
             </select>
             <button onclick="submitForm()">Submit</button>
             <button onclick="closeSubmissionForm()">Cancel</button>
